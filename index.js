5f40ca4807efd676802b925209b346b9ba100317
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-14 10:33:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const express = require('express');
+const http = require('http');
 const logger = require('./src/utils/logger');
 const bot = require('./src/bot');
 const config = require('./src/config');
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
 
 const startBot = async () => {
   try {
@@ -12,8 +21,23 @@ const startBot = async () => {
     await bot.launch();
     logger.info(`${config.botName} is running`);
 
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+    const port = process.env.PORT || 3000;
+    server.listen(port, () => {
+      logger.info(`Server is running on port ${port}`);
+    });
+
+    setInterval(() => {
+      http.get(`http://localhost:${port}`);
+    }, 5 * 60 * 1000);
+
+    process.once('SIGINT', () => {
+      bot.stop('SIGINT');
+      server.close();
+    });
+    process.once('SIGTERM', () => {
+      bot.stop('SIGTERM');
+      server.close();
+    });
   } catch (error) {
     logger.error('Error starting the bot:', error);
     process.exit(1);
